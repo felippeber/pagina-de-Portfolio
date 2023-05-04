c598a6392e136e026c6b7e47290e9134bbd8ad3d
felippeber <felippeber@gmail.com>
2023-05-04 16:18:39 -0300

    atualização do link

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
-async function fetchProfileDate() {
-    const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json'
+async function fetchProfileData() {
+    const url = 'https://raw.githubusercontent.com/felippeber/js-developer-portfolio/main/data/profile.json'
     const fetching = await fetch(url)
     return await fetching.json()
 }
