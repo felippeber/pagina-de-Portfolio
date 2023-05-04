8018c13301cf8c291db8591a48b2e2064d57b90e
felippeber <felippeber@gmail.com>
2023-05-04 16:50:11 -0300

    novo local

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/felippeber/P-gina-de-Portfolio/master/assets/data/profile.json?token=GHSAT0AAAAAACCGBMJLERPKIRJR5JYOOQ3GZCUBGCQ'
+    const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json'
     const fetching = await fetch(url)
     return await fetching.json()
 }
