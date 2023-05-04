6f1bd974293b1a817b21c49b9b630f47db9ef500
felippeber <felippeber@gmail.com>
2023-05-04 16:22:39 -0300

    atualizando o link

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/felippeber/js-developer-portfolio/main/data/profile.json'
+    const url = 'https://raw.githubusercontent.com/felippeber/P-gina-de-Portfolio/master/assets/data/profile.json?token=GHSAT0AAAAAACCGBMJKSJ2BF66JSF4CHBSCZCUATOQ'
     const fetching = await fetch(url)
     return await fetching.json()
 }
