c2eff7adaa4653d9eb3ab2d83726ec2ec8db55b6
felippeber <felippeber@gmail.com>
2023-05-08 17:37:28 -0300

    Auterações no main portfolio e global

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,8 +42,8 @@ function updatePortfolio(profileData){
     portfolio.innerHTML = profileData.portfolio.map(project => {
         return `
             <li>
-                <h3 ${project.github ? 'class="github"' : ''}>${project.name}
-                <a href="${project.url}" target="_blank">${project.url}
+                <h3 ${project.github ? 'class="github"' : ''}>${project.name}</h3>
+                <a href="${project.url}" target="_blank">${project.url}</a>
             </li>
         `
     }).join('')
